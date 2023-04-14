d66fe904e0cb2f2fa550e28e84910adecabd9ec6
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-14 18:29:37 +0530

    fresh start with vite init

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import "./App.css";
-import { useState } from "react";
-
-import InputCard from "./InputCard";
-import DisplayCard from "./DisplayCard";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  // Initialize state
-  const [users, setState] = useState([
-    { id: 1, name: "Joe", age: 34 },
-    { id: 2, name: "Jane", age: 29 },
-  ]);
-
-  const addNewUser = (newUser) => {
-    setState((prevState) => [...prevState, newUser]);
-  };
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <InputCard addNewUser={addNewUser} />
-      <DisplayCard users={users} />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
