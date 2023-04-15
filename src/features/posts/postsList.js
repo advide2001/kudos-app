6e979a4341cbbcd4d1e92a45c1e06135f06bfe22
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-15 22:56:13 +0530

    Showing posts on different pages using routing

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const PostsList = () => {
   // fetch posts from the store
@@ -9,6 +10,9 @@ export const PostsList = () => {
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
     </article>
   ))
 
