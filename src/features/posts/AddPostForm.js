091e094c12c134f1663b6ef6419ef3a7efe4c1a6
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-16 10:52:05 +0530

    Prepared action payloads for postAdded reducer function

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { postAdded } from './postsSlice'
 import { useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
@@ -14,13 +13,7 @@ export const AddPostForm = () => {
 
   const onSavePostAdded = () => {
     if (title && content) {
-      dispatch(
-        postAdded({
-          id: nanoid(),
-          title,
-          content,
-        })
-      )
+      dispatch(postAdded(title, content))
       setTitle('')
       setContent('')
     }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { nanoid } from 'nanoid'
 
 const initialState = [
   {
@@ -19,8 +20,19 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        }
+      },
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
