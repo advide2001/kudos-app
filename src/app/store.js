6d4032281647b14001852fa93d8b094ca27e4301
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-07 20:56:23 +0530

    clear repo, fresh start

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import postsReducer from '../features/posts/postsSlice'
-
-export default configureStore({
-  reducer: {
-    posts: postsReducer,
-  },
-})
