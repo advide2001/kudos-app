da983226e92c9d7b604927c0d99a7948a8f613ef
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-10 23:05:34 +0530

    feat: Log some data to test clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 
-export default function Home() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+export default async function Home() {
+  const { userId } = auth();
+  const user = await currentUser();
+  console.log(userId);
+  console.log(user);
+  return <h1>Hello World</h1>;
 }
